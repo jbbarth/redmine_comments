7419c6c07fea4b4af15ca6a61f6f5fdadf362aef
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-14 18:04:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Redmine Comments plugin
+=======================
+
+This plugin adds private comments in Redmine issues. You can manage comments
+visibility through roles and permissions. A typical usage for an tech
+organization is to allow staff users to see/edit comments, but not lambda
+users.
---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_comments do
+  name 'Redmine Comments plugin'
+  description 'Private comments in issues for staff users'
+  author 'Jean-Baptiste BARTH'
+  author_url 'mailto:jeanbaptiste.barth@gmail.com'
+  version '0.0.1'
+  url 'https://github.com/jbbarth/redmine_base_jquery'
+end
