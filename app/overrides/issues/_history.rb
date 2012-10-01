864bf56d793d39b590da23b214b50647a1e6055d
jbbarth <jeanbaptiste.barth@gmail.com>
2012-10-01 22:14:28 +0200

    Respect user preference for comments order

--- app/overrides/issues/_history.rb
+++ app/overrides/issues/_history.rb
@@ -1,7 +1,7 @@
 Deface::Override.new :virtual_path  => 'issues/_history',
                      :name          => 'add-comments-to-journals',
                      :insert_before => 'code[erb-silent]:contains("journal in journals")',
-                     :text          => '<% journals = (journals + @issue.comments).sort_by(&:created_on) if User.current.allowed_to?(:view_private_comments, @project) %>'
+                     :text          => '<% if User.current.allowed_to?(:view_private_comments, @project); journals = (journals + @issue.comments).sort_by(&:created_on); journals.reverse! if User.current.wants_comments_in_reverse_order?; end %>'
 
 Deface::Override.new :virtual_path  => 'issues/_history',
                      :name          => 'display-comments-with-journals',
