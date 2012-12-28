6dc34abe5d3687412240c961724c7882992b9e6a
jbbarth <jeanbaptiste.barth@gmail.com>
2012-12-28 08:02:34 +0100

    Back to basic test/unit syntax in test/unit/issue_test.rb, was breaking randomly because of missing shoulda context

--- test/unit/issue_test.rb
+++ test/unit/issue_test.rb
@@ -3,19 +3,15 @@ require File.dirname(__FILE__) + '/../test_helper'
 class IssueTest < ActiveSupport::TestCase
   fixtures :users, :issues, :issue_statuses, :projects, :projects_trackers, :trackers, :enumerations
 
-  context "Issue#comments" do
-
-    should "have comments" do
-      issue = Issue.first
-      assert_equal [], issue.comments
-      assert_equal 0, issue.comments_count
-      assert_difference 'issue.comments.count', +1 do
-        Comment.create!(:commented => issue, :comments => "Blah", :author => User.find(1))
-      end
-      issue.reload
-      assert_equal 1, issue.comments_count
-      assert_equal "Blah", issue.comments.first.comments
+  test "Issue#comments basic behaviour" do
+    issue = Issue.first
+    assert_equal [], issue.comments
+    assert_equal 0, issue.comments_count
+    assert_difference 'issue.comments.count', +1 do
+      Comment.create!(:commented => issue, :comments => "Blah", :author => User.find(1))
     end
-
+    issue.reload
+    assert_equal 1, issue.comments_count
+    assert_equal "Blah", issue.comments.first.comments
   end
 end
