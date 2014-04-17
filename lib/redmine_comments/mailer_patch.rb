47808528a6fc9868119b4d4aa0636645bb77dd75
Vincent Robert <vincent.robert@nanego.com>
2014-04-17 09:03:17 +0200

    Fix compatibility with redmine 2.5.1

--- lib/redmine_comments/mailer_patch.rb
+++ lib/redmine_comments/mailer_patch.rb
@@ -10,13 +10,13 @@ class Mailer
     message_id comment
     references issue
     @author = comment.author
-    recipients = issue.comment_recipients
+    @recipients = issue.comment_recipients
     #subject
     s = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}][#{l(:label_comment).upcase}] "
     s << issue.subject
     @issue = issue
     @comment = comment
     @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue, :anchor => "comment-#{comment.id}")
-    mail :to => recipients, :subject => s
+    mail :to => @recipients, :subject => s
   end
 end
