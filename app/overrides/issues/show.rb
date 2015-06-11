f1416a097ad70e16d5e70f784377561e705cced2
Vincent Robert <vincent.robert@nanego.com>
2015-06-11 11:03:42 +0200

    'all' method now returns an ActiveRecord Relation and not an array

--- app/overrides/issues/show.rb
+++ app/overrides/issues/show.rb
@@ -1,4 +1,9 @@
 Deface::Override.new :virtual_path  => 'issues/show',
                      :name          => 'reorder-journals-in-show-issue',
                      :insert_before => 'h2',
-                     :text          => '<% @journals.sort_by!(&:created_on); @journals.reverse! if User.current.wants_comments_in_reverse_order?; %>'
+                     :text          => <<EOS
+<%
+  @journals.order('created_on');
+  @journals.reverse! if User.current.wants_comments_in_reverse_order?;
+%>
+EOS
