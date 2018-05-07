fb02e89ae7fd29f534ca78a35a0d163802f05465
Vincent Robert <vincent.robert@nanego.com>
2018-05-07 12:31:41 +0200

    Quick fix to restore inline images in views

--- lib/redmine_comments/journal_patch.rb
+++ lib/redmine_comments/journal_patch.rb
@@ -3,8 +3,6 @@ require_dependency 'journal'
 class Journal
   acts_as_attachable
 
-  remove_method :attachments
-
   def journal_attachments
     Attachment.where(container_id: self.id, container_type: Journal)
   end
