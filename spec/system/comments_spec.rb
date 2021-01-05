698f934d3fde13e978c8ddbc3c5fce374c863ea1
Vincent Robert <vincent.robert@nanego.com>
2021-01-05 15:21:04 +0100

    Complete system specs with visibility options

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -26,11 +26,22 @@ describe "creating new comments", type: :system do
            :watchers, :journals, :journal_details, :versions,
            :workflows
 
+  let!(:project) { Project.find(1) }
+  let!(:issue) { Issue.find(1) }
+  let!(:user_jsmith) { User.find(2) }
+  let!(:membership) { Member.find(1) }
+
+  if Redmine::Plugin.installed?(:redmine_limited_visibility)
+    let!(:function_1) { Function.find(1) }
+    let!(:function_2) { Function.find(2) }
+    let!(:function_3) { Function.find(3) }
+    let!(:function_4) { Function.find(4) }
+    let!(:function_5) { Function.find(5) }
+  end
+
   before do
     log_user('jsmith', 'jsmith')
   end
-
-  let!(:issue_1) { Issue.find(1) }
 
   it "creates a comment without advanced options" do
     expect {
@@ -42,13 +53,40 @@ describe "creating new comments", type: :system do
         fill_in 'journal[notes]', with: 'Here is a quick note'
         click_on 'Add'
       end
-    }.to change(issue_1.journals, :size).by(1)
+    }.to change(issue.journals, :size).by(1)
 
-    journal = issue_1.journals.last
+    journal = issue.journals.last
     expect(journal.private_notes).to be_truthy
     expect(journal.notes).to eq 'Here is a quick note'
     expect(journal.roles).to be_empty
   end
 
-end
+  if Redmine::Plugin.installed?(:redmine_limited_visibility)
 
+    it "creates a comment with limited visibility by function" do
+      Role.find_by_name('Manager').add_permission! :view_private_notes_from_role_or_function
+      membership.functions = [function_1, function_2, function_3]
+
+      expect {
+        visit '/issues/1'
+        click_on "Comment", match: :first
+        within 'form#add_comment_form' do
+          expect(page).to have_selector(".info", text: "Private comments are only visible with specific roles.")
+          expect(page).to have_selector(".comment_role", text: "function1")
+          fill_in 'journal[notes]', with: 'Here is a quick note'
+          page.find('.comment_role', text: 'function1').click
+          page.find('.comment_role', text: 'function2').click
+          click_on 'Add'
+        end
+      }.to change(issue.journals, :size).by(1)
+
+      journal = issue.journals.last
+      expect(journal.private_notes).to be_truthy
+      expect(journal.notes).to eq 'Here is a quick note'
+      expect(journal.roles).to be_empty
+      expect(journal.functions).to eq [function_3]
+    end
+
+  end
+
+end
