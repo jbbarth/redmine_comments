84029a5e1875f7fb366acf039761a7287de5befd
Vincent Robert <vincent.robert@nanego.com>
2021-01-27 11:43:55 +0100

    Fix comment edit form

--- app/overrides/journals/_notes_form.rb
+++ app/overrides/journals/_notes_form.rb
@@ -2,5 +2,5 @@ Deface::Override.new :virtual_path => 'journals/_notes_form',
                      :name => 'add-visibility-fields-to-private-note-form',
                      :insert_after => "erb[loud]:contains(\"form_tag\")",
                      :text => <<-EOS
-<%= render "issue_comments/visibility_form_fields" %>
+<%= render "issue_comments/visibility_form_fields" if @journal.private_notes %>
 EOS
